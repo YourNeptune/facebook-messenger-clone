1053191e8d6e18e8b66021f5c3fd7ce7d7ad5357
Lixuan Luo <lixuanluo@hotmail.com>
2021-04-06 22:03:02 -0400

    styling user message & guest message

--- src/App.js
+++ src/App.js
@@ -1,31 +1,42 @@
-import { Button, FormControl, FormHelperText, Input, InputLabel } from '@material-ui/core';
-import { useState } from 'react';
-import './App.css';
+import { Button, FormControl, Input, InputLabel } from "@material-ui/core";
+import { useState } from "react";
+import "./App.css";
+import Message from "./Components/Message";
 
 function App() {
-  const [input, setInput] = useState('')
-  const [messages, setMessages] = useState(['msg1', 'msg2', 'msg3'])
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([
+    { username: "User", text: "Hello" },
+    { username: "Guest", text: "Hi!" },
+  ]);
+  const [username, setUsername] = useState("User");
 
-  const sendMessages = e => {
-    e.preventDefault()
-    setMessages([...messages, input])
-    setInput('')
-  }
+  const sendMessages = (e) => {
+    e.preventDefault();
+    setMessages([...messages, { username: username, text: input }]);
+    setInput("");
+  };
 
   return (
     <div className="App">
-    <h1>Facebook Messages Clone</h1>
+      <h1>Facebook Messages Clone</h1>
       <form>
         <FormControl>
-          <InputLabel>Email address</InputLabel>
-          <Input value={input} onChange={e => setInput(e.target.value)} />
+          <InputLabel>Enter a message</InputLabel>
+          <Input value={input} onChange={(e) => setInput(e.target.value)} />
         </FormControl>
-        <Button variant="contained" color="primary" type='submit'  onClick={sendMessages} disabled={!input}>Send</Button>
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+          onClick={sendMessages}
+          disabled={!input}
+        >
+          Send
+        </Button>
       </form>
-      {messages.map(msg => {
-        return (
-          <p>{msg}</p>
-        )
+      {messages.map((msg) => {
+        return <Message username={username} msg={msg} />;
       })}
     </div>
   );
---
+++ src/Components/Message.js
@@ -0,0 +1,19 @@
+import { Card, CardContent, Typography } from "@material-ui/core";
+
+const Message = ({ username, msg }) => {
+  const isUser = username === msg.username;
+
+  return (
+    <div className={`msg ${isUser && "msg_user"}`}>
+      <Card className={isUser ? "msg_userCard" : "msg_guestCard"}>
+        <CardContent>
+          <Typography variant="h5" component="h2">
+            {msg.username}: {msg.text}
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default Message;
