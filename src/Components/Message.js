b13ac714cac5b5098987723ad8ebc3b67729375e
Lixuan Luo <lixuanluo@hotmail.com>
2021-04-08 19:36:05 -0400

    styles

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -5,17 +5,19 @@ const Message = forwardRef(({ username, data }, ref) => {
   const isUser = username === data.username;
 
   return (
-    <div ref={ref} className={`msg ${isUser && "msg_user"}`}>
-      <p>{data.username || "Unknown User"}</p>
-      <Card className={isUser ? "msg_userCard" : "msg_guestCard"}>
-        <CardContent>
-          <Typography variant="h6">
-            {/* {isUser ? `${data.text}` : `${data.username} : ${data.text}`} */}
-            {data.text}
-          </Typography>
-        </CardContent>
-      </Card>
-    </div>
+    <>
+      <div ref={ref} className={`msg ${isUser && "msg_user"}`}>
+        <p className="username">{data.username || "Unknown User"}</p>
+        <Card className={`${isUser ? "msg_userCard" : "msg_guestCard"}`}>
+          <CardContent>
+            <Typography variant="h6">
+              {/* {isUser ? `${data.text}` : `${data.username} : ${data.text}`} */}
+              {data.text}
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
+    </>
   );
 });
 
