194bf4ab9477a59747ce69a29c860c829cee09f6
Lixuan Luo <lixuanluo@hotmail.com>
2021-05-21 14:51:49 -0400

    add a tooltip

--- src/Components/UsernameForm.js
+++ src/Components/UsernameForm.js
@@ -2,18 +2,30 @@ import { Button, TextField } from "@material-ui/core";
 
 const UsernameForm = ({username, onChange, onSend}) => {
     return (
-      <form className="form_container">
-        <TextField
-          label="Enter your username"
-          variant="outlined"
-          value={username}
-          onChange={onChange}
-          className='username__input'
-        />
-        <Button className='username__btn' type='submit' variant="contained" color="primary" onClick={onSend}>
-          Chat Now
-        </Button>
-      </form>
+      <div>
+        <form className="form_container tooltip">
+          <span className="tooltiptext">
+            Please enter a username to start chatting or chat as an unknown user
+            without an username.
+          </span>
+          <TextField
+            label="Enter your username"
+            variant="outlined"
+            value={username}
+            onChange={onChange}
+            className="username__input"
+          />
+          <Button
+            className="username__btn "
+            type="submit"
+            variant="contained"
+            color="primary"
+            onClick={onSend}
+          >
+            Chat Now
+          </Button>
+        </form>
+      </div>
     );
 }
 
